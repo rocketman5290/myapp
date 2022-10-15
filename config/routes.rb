b30aa46f5ed12b2eea2afb3ce3fcb188cbfe2ca2
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-10-15 13:19:13 -0400

    AddRoutes: Job routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  namespace :api do
+    namespace :v1 do
+      get '/jobs', to: 'jobs#index'
+      post '/jobs', to: 'jobs#create'
+      get '/jobs/:id', to: 'jobs#show'
+      patch '/jobs/:id', to: 'jobs#update'
+      delete '/jobs/:id', to: 'jobs#destroy'
+    end
+  end
 end
