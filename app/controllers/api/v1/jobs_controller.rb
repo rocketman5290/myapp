14f1d8246125ff591340a0d35857e45b836619aa
WILLIAM GEORGE <ROCKETMAN5290@GMAIL.com>
2022-10-15 13:18:21 -0400

    AddController: Jobs_Controller

---
+++ app/controllers/api/v1/jobs_controller.rb
@@ -0,0 +1,44 @@
+module Api::V1
+    class JobsController < ApplicationController
+        
+        def index
+            @jobs = Job.all
+            render json: @jobs
+        end
+
+        def show
+            @jobs = Job.find(params[:id])
+            render json: @jobs
+        end
+
+        def create
+            @jobs = Job.new(job_params)
+            if @jobs.save
+                render json: @jobs, status: :created
+            else
+                render json: @jobs.errors, status: :unprocessable_entity
+            end
+        end
+
+        def update
+            @jobs = Job.find(params[:id])
+            if @jobs.update(job_params)
+                render json: @jobs
+            else
+                render json: @jobs.errors, status: :unprocessable_entity
+            end
+        end
+
+        def destroy
+
+        end
+
+
+        private
+        
+        def job_params
+            params.require(:job).permit(:company, :position, :description)
+        end
+
+    end
+end
